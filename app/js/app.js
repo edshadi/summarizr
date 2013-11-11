2def1a2f843e00e90ff67ea258953643ece786f8
Ed Shadi <edshadi@gmail.com>
2013-11-10 19:10:04 -0800

    add getting started as index page.

--- app/js/app.js
+++ app/js/app.js
@@ -10,7 +10,8 @@ angular.module('Summarizr', [
   'Summarizr.controllers'
 ]).
 config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/getting-started', {templateUrl: 'partials/getting-started.html', controller: 'StartCtrl'});
   $routeProvider.when('/give-summary', {templateUrl: 'partials/give-summary.html', controller: 'GiveCtrl'});
   $routeProvider.when('/need-summary', {templateUrl: 'partials/need-summary.html', controller: 'NeedCtrl'});
-  $routeProvider.otherwise({redirectTo: '/give-summary'});
+  $routeProvider.otherwise({redirectTo: '/getting-started'});
 }]);
